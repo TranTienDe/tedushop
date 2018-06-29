1fd5b0f1cfd7cc2e5b77670263c461c1f01746e1
TranTienDe <sakurahonni@gmail.com>
2018-06-29 13:35:34 +0700

    add model data

---
+++ TeduShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace TeduShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private TeduShopDBContext dbContext;
+
+        public TeduShopDBContext Init()
+        {
+            return dbContext ?? (dbContext = new TeduShopDBContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+            {
+                dbContext.Dispose();
+            }
+        }
+    }
+}
