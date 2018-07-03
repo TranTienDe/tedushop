c83c7cf161b0ebed41014b1fac0702864854db18
TranTienDe <sakurahonni@gmail.com>
2018-07-03 14:26:26 +0700

    mapper

---
+++ TeduShop.Web/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TeduShop.Model.Models;
+using TeduShop.Web.Models;
+
+namespace TeduShop.Web.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Post, PostViewModel>();
+                cfg.CreateMap<PostCategory, PostCategoryViewModel>();
+                cfg.CreateMap<Tag, TagViewModel>();
+                cfg.CreateMap<ProductCategory, ProductCategoryViewModel>();
+                cfg.CreateMap<Product, ProductViewModel>();
+                cfg.CreateMap<ProductTag, ProductTagViewModel>();                          
+            });
+
+        }
+    }
+}
