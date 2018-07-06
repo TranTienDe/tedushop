72238bac3db53cbeed872c0aa6fe162db8ff9e3e
TranTienDe <sakurahonni@gmail.com>
2018-07-06 16:13:00 +0700

    - Them dinh dang date , currency va filter. - Them button icon.

---
+++ TeduShop.Web/app/shared/filter/statusFilter.js
@@ -0,0 +1,11 @@
+﻿(function (app) {
+
+    app.filter('statusFilter', function () {
+
+        return function (input) {
+            return input ? 'Kích hoạt' : 'Đã khóa'
+        }
+
+    });
+
+})(angular.module('tedushop.common'));
